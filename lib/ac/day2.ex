ee6115dc72214dcbc93119df454f0758150b5c50
Dan Bowles <daniel.bowles@gmail.com>
2024-12-26 10:54:57 -0500

    :white_check_mark: Day 2 part 1 done

--- lib/ac/day2.ex
+++ lib/ac/day2.ex
@@ -1,17 +1,39 @@
 defmodule AC.Day2 do
   def load_data() do
-    a =
-      File.read!("priv/2024/day2.txt")
-      |> String.trim()
-      |> String.split("\n")
-      |> Enum.map(&String.split(&1, ~r/\s+/, trim: true))
-      |> Enum.map(&Enum.map(&1, fn a -> String.to_integer(a) end))
+    File.read!("priv/2024/day2.txt")
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_line/1)
+  end
 
-    # |> Enum.map(fn [a, b] -> {String.to_integer(a), String.to_integer(db)} end)
+  defp parse_line(line) do
+    line
+    |> String.split(~r/\s+/, trim: true)
+    |> Enum.map(&String.to_integer/1)
+  end
 
-    # |> List.zip()
-    # |> Enum.map(&Tuple.to_list/1)
+  defp with_neighbor(l) do
+    Enum.zip(l, Enum.slice(l, 1..-1//1))
+  end
 
-    IO.inspect(a |> Enum.at(0))
+  defp pattern_test([h | [th | _]]) when h > th, do: &Kernel.>/2
+  defp pattern_test([h | [th | _]]) when h < th, do: &Kernel.</2
+  defp pattern_test(_), do: &Kernel.==/2
+
+  def part_one(data) do
+    data |> Enum.count(&safe_record/1)
+  end
+
+  defp safe_record(data_row) do
+    pattern = pattern_test(data_row)
+    IO.inspect(data_row, charlists: :lists)
+
+    IO.inspect(
+      data_row
+      |> with_neighbor()
+    )
+
+    data_row
+    |> with_neighbor()
+    |> Enum.all?(fn {a, b} -> pattern.(a, b) and abs(a - b) in 1..3 end)
   end
 end
